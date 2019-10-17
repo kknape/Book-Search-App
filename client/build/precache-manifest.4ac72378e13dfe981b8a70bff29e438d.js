ad1e19441be6afabdbe153d6d4cef55f046a4dbc
k2k <kadonna@wonderkul.com>
2019-10-16 22:26:12 -0500

    Added build folder

---
+++ client/build/precache-manifest.4ac72378e13dfe981b8a70bff29e438d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "458f940999002e6ef781",
+    "url": "/static/js/main.4dd716b9.chunk.js"
+  },
+  {
+    "revision": "5be9ae5c3b0f648d690a",
+    "url": "/static/js/2.d161bde6.chunk.js"
+  },
+  {
+    "revision": "458f940999002e6ef781",
+    "url": "/static/css/main.5d04b10c.chunk.css"
+  },
+  {
+    "revision": "3cec175c0b67516e8d37edb952f97b2e",
+    "url": "/index.html"
+  }
+];
